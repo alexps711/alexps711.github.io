17667de1227c0a6d163134ad7fb6521b244a9038
alexps711 <alejanper@hotmail.com>
2021-05-23 23:30:50 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Typist from 'react-typist';
+import github from './assets/github.png';
+import linkedin from './assets/linkedin.png';
+
 
 function App() {
+  const [logosShowing, showLogos] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Typist startDelay={500} onTypingDone={() => setTimeout(() => {
+          showLogos(true)
+        }, 300)}>
+          Alejandro Perez
+        </Typist>
+        <br />
+        {logosShowing ?
+          <div className="Logos">
+            <a href="https://github.com/alexps711" target="_blank">
+              <img src={github} />
+            </a>
+            &nbsp;
+            <a href="https://www.linkedin.com/in/alejandro-perez-salas/" target="_blank">
+              <img src={linkedin} />
+            </a>
+          </div>
+          : null}
       </header>
     </div>
   );
